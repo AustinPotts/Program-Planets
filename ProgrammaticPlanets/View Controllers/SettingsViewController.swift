56db2cda67f5ad753ca63caae83a68a195326949
AustinPotts <pottsevolvements@gmail.com>
2019-10-07 13:41:18 -0400

    Configured Pluto Switch, Label, Button

--- ProgrammaticPlanets/View Controllers/SettingsViewController.swift
+++ ProgrammaticPlanets/View Controllers/SettingsViewController.swift
@@ -15,6 +15,7 @@ class SettingsViewController: UIViewController {
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
+        setUpSubViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,16 +25,79 @@ class SettingsViewController: UIViewController {
     
     // MARK: - Action Handlers
     
-    func changeShouldShowPluto(_ sender: UISwitch) {
+   @objc func changeShouldShowPluto(_ sender: UISwitch) {
         let userDefaults = UserDefaults.standard
         userDefaults.set(sender.isOn, forKey: .shouldShowPlutoKey)
     }
     
-    func done() {
+   @objc func done() {
         dismiss(animated: true, completion: nil)
     }
     
     // MARK: - Private
+    
+    private func setUpSubViews(){
+        //Set up the way the UI Looks
+        let doneButton = UIButton(type: .system)
+        doneButton.setTitle("Done", for: .normal)
+        doneButton.addTarget(self, action: #selector(done), for: .touchUpInside)
+        
+        
+        //Add as subview
+        view.addSubview(doneButton)
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Constrain
+        let doneTopConstraint = NSLayoutConstraint(item: doneButton,
+                                                   attribute: .top,
+                                                   relatedBy: .equal,
+                                                   toItem: view.safeAreaLayoutGuide,
+                                                   attribute: .top,
+                                                   multiplier: 1,
+                                                   constant: 20)
+        
+        
+        let doneButtonTrailing =  NSLayoutConstraint(item: doneButton,
+                                                     attribute: .trailing,
+                                                     relatedBy: .equal,
+                                                     toItem: view.safeAreaLayoutGuide,
+                                                     attribute: .trailing,
+                                                     multiplier: 1,
+                                                     constant: -20)
+        
+        NSLayoutConstraint.activate([doneTopConstraint,
+                                            doneButtonTrailing])
+        
+        
+        //Create UI
+        let plutoSwitch = UISwitch()
+        plutoSwitch.addTarget(self, action: #selector(changeShouldShowPluto), for: .valueChanged)
+        
+        //Constrain UI
+        view.addSubview(plutoSwitch)
+        plutoSwitch.translatesAutoresizingMaskIntoConstraints = false
+        
+        plutoSwitch.topAnchor.constraint(equalTo: doneButton.bottomAnchor, constant: 40).isActive = true
+        plutoSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        
+        //Set the view controllers property to the switch we just set up
+        self.shouldShowPlutoSwitch = plutoSwitch
+        
+        let label = UILabel()
+        label.text = "Is Pluto a Planet"
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.centerYAnchor.constraint(equalTo: plutoSwitch.centerYAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        label.trailingAnchor.constraint(equalTo: plutoSwitch.leadingAnchor, constant: -8).isActive = true
+        
+        
+        
+    }
+    
+    
     
     private func updateViews() {
         let userDefaults = UserDefaults.standard
