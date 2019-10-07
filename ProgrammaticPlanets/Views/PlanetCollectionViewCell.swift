58388a8c90d92f1b5324d3c6d7105e6a1349047a
AustinPotts <pottsevolvements@gmail.com>
2019-10-07 14:02:21 -0400

    Configured Custom Collection View

--- ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
+++ ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
@@ -19,6 +19,82 @@ class PlanetCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    
+    // You programatically initializing  this cell
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubViews()
+    }
+    
+    
+    private func setUpSubViews(){
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        self.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+                            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 4),
+                            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 2),
+                            imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -2),
+                            //1:1 anchor making the height equal the width
+                            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1)
+            
+            
+                                    ])
+        
+                    self.imageView = imageView
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+            NSLayoutConstraint(item: label,
+                               attribute: .top,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .bottom,
+                               multiplier: 1,
+                               constant: 4),
+            
+            NSLayoutConstraint(item: label,
+                               attribute: .leading,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .leading,
+                               multiplier: 1,
+                               constant: 2),
+            
+            NSLayoutConstraint(item: label,
+                               attribute: .trailing,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .trailing,
+                               multiplier: 1,
+                               constant: -2)
+            
+            
+            
+        
+        ])
+        
+        self.nameLabel = label
+        
+        
+    }
+    
+    
     private func updateViews() {
         imageView.image = planet.image
         nameLabel.text = planet.name
